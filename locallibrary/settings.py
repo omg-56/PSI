cc6c2e2ffd2c0ac4b9f211136cc562492576fd18
pingu9191 <nachonugom@gmail.com>
2024-02-20 20:09:09 +0100

    change static

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -148,10 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "catalog/static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
